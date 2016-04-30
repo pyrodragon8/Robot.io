58a806a0e70ddd3596c8e65ce0c34ee10063ca5d
pyrodragon8 <pyrodragon8@gmail.com>
2016-04-30 12:11:30 +1200

    Color, Dimension and Joystick event Addition

--- app/src/main/java/inc/ly/robot/robotio/MainActivity.java
+++ app/src/main/java/inc/ly/robot/robotio/MainActivity.java
@@ -2,6 +2,10 @@ package inc.ly.robot.robotio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jmedeisis.bugstick.Joystick;
+import com.jmedeisis.bugstick.JoystickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+
+        Joystick joystick = (Joystick) findViewById(R.id.joystick);
+        joystick.setJoystickListener(new JoystickListener() {
+            @Override
+            public void onDown() {
+                Log.d("JoyStickOP", "onDown: ");
+            }
+
+            @Override
+            public void onDrag(float degrees, float offset) {
+                Log.d("JoyStickOP", "onDrag: ");
+            }
+
+            @Override
+            public void onUp() {
+                Log.d("JoyStickOP", "onUp: ");
+            }
+        });
+
+
+
+
+
     }
+
+
+
+
+
 }
